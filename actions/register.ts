16097d43e01c54b6da1f30a43038eb7254a0337c
Belendia <belendia@gmail.com>
2025-02-25 11:53:38 +0300

    Add user registration

--- actions/register.ts
+++ actions/register.ts
@@ -1,46 +1,14 @@
 "use server";
 
 import * as z from "zod";
-// import bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 
-// import { db } from "@/lib/db";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByUsername } from "@/lib/data";
 // import { getUserByEmail } from "@/data/user";
 // import { sendVerificationEmail } from "@/lib/mail";
 // import { generateVerificationToken } from "@/lib/tokens";
-
-// export const register = async (values: z.infer<typeof RegisterSchema>) => {
-//   const validatedFields = RegisterSchema.safeParse(values);
-
-//   if (!validatedFields.success) {
-//     return { error: "Invalid fields!" };
-//   }
-
-//   const { email, password, name } = validatedFields.data;
-//   const hashedPassword = await bcrypt.hash(password, 10);
-
-//   const existingUser = await getUserByEmail(email);
-
-//   if (existingUser) {
-//     return { error: "Email already in use!" };
-//   }
-
-//   await db.user.create({
-//     data: {
-//       name,
-//       email,
-//       password: hashedPassword,
-//     },
-//   });
-
-//   const verificationToken = await generateVerificationToken(email);
-//   await sendVerificationEmail(
-//     verificationToken.email,
-//     verificationToken.token,
-//   );
-
-//   return { success: "Confirmation email sent!" };
-// };
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -49,5 +17,28 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Login successful!" };
+  const { username, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByUsername(username);
+
+  if (existingUser) {
+    return { error: "Username already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      username,
+      password: hashedPassword,
+    },
+  });
+
+  // const verificationToken = await generateVerificationToken(email);
+  // await sendVerificationEmail(
+  //   verificationToken.email,
+  //   verificationToken.token,
+  // );
+
+  return { success: "User created!" };
 };
