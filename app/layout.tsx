c2ae4856851e1f0411ee35ecc610fe98e3c83d6c
Belendia <belendia@gmail.com>
2025-03-18 12:14:46 +0300

    Add logic to protect api and server actions based on role

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import LanguageSwitcher from "@/components/language-switcher";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default async function RootLayout({
           <div className="flex p-2 justify-end">
             <LanguageSwitcher />
           </div>
+          <Toaster />
 
           {children}
         </body>
