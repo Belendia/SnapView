3985e66b2e1aef3714921cf60cb5259aea64dbdf
Belendia <belendia@gmail.com>
2025-04-04 18:26:50 +0300

    Moved the code to src

--- MiniApp/next.config.ts
+++ MiniApp/next.config.ts
@@ -3,7 +3,7 @@ import "@/lib/env-check";
 
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin("./lib/i18n/i18n.ts");
+const withNextIntl = createNextIntlPlugin("./src/lib/i18n/i18n.ts");
 
 const nextConfig: NextConfig = {};
 
